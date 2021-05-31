15949cc43e3a6949daf3ae2e9a63d3c873c9ec43
Richard <37025447+richiedevs@users.noreply.github.com>
2021-05-31 02:25:27 +0200

    Update client.js

--- src/client.js
+++ src/client.js
@@ -115,7 +115,7 @@ class RPCClient extends EventEmitter {
           e.reject(new Error('connection closed'));
         });
         this.emit('disconnected');
-        console.error(new Error('connection closed'));
+        console.error('connection closed');
       });
       this.transport.connect().catch(reject);
     });
